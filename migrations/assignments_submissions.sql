d5cbcc308ad247203555bd5d139c7aa07f0b459a
stanw0ng <user.stanley@gmail.com>
2023-04-03 16:38:13 -0400

    added inserted many lines into assignment entities

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -7,7 +7,7 @@ CREATE TABLE assignments (
   duration INTEGER
 );
 
-CREATE TABLE assignments_submissions (
+CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
