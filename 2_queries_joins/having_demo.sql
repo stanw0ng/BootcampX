d48fb1837d10193741ef599de31377101fccce1d
stanw0ng <user.stanley@gmail.com>
2023-04-03 19:30:32 -0400

    added queries which utilize joins

---
+++ 2_queries_joins/having_demo.sql
@@ -0,0 +1,6 @@
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;  /* HAVING is evaluated before SELECT so we can't use the alias */
