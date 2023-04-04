31892cad1cd478746550d917160225e7e682c4f7
stanw0ng <user.stanley@gmail.com>
2023-04-03 21:37:24 -0400

    added queries thats utilize GROUP BY

---
+++ 3_queries_group-by/average_completion_time.sql
@@ -0,0 +1,7 @@
+SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration
+FROM assignment_submissions
+JOIN students on students.id = student_id
+JOIN assignments on assignments.id = assignment_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY average_assignment_duration DESC;
